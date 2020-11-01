2f30b1e4eac42225b0828fbdc9010b8cd51869e7
Javer Parra <jparra305@unab.edu.co>
2020-11-01 17:58:57 -0500

    vjhjh

--- Server.js
+++ Server.js
@@ -147,12 +147,3 @@ app.put('/PepData/put/:coleccion/:username/:password/:tiempo/:puntuacion/:catego
     res.send("Se ha producido un error al actualizar PepData");
   }  
 })
-
-if(process.env.NODE_ENV === 'production'){
-  //Static folder
-  app.use(express.static(dirname + '/public'));
-
-app.get(/.*/ , (req, res) => res.sendFile(dirname + '/public/index.html')); 
-//El man lo hace para manejar la SPA 
-}
-const port = process.env.PORT || 3000;
